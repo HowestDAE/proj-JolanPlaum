85225fcd622385ca70c1fdf8af146ba81206cbef
Jolan Plaum <jolan.plaum@student.howest.be>
2023-06-01 21:41:28 +0200

    Create Models for the api
    In animal crossing there are 3 types of critters that can be collected: bugs, fish and sea creatures.
    They have overlapping data, but also some unique data, which is why they all inherit from a base class called BaseCritter.
    The unique data will be added later on, for now I'm focusing on making everything work.

---
+++ AnimalCrossing/Model/BaseCritter.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalCrossing.Model
+{
+	public abstract class BaseCritter
+	{
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("name.name-EUen")]
+		public string Name { get; set; }
+
+		[JsonProperty("price")]
+		public int Price { get; set; }
+
+		[JsonProperty("availability.month-array-northern")]
+		public List<int> Months { get; set; }
+
+		[JsonProperty("availability.time-array")]
+		public List<int> Times { get; set; }
+
+		[JsonIgnore]
+		public abstract string Type { get; }
+
+		[JsonProperty]
+		public string UrlEndpoint
+		{
+			get
+			{
+				return $"/{Type}/{Id}";
+			}
+		}
+	}
+}
---
+++ AnimalCrossing/Model/SeaCritter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalCrossing.Model
+{
+	public class SeaCritter : BaseCritter
+	{
+		public override string Type { get { return "sea"; } }
+	}
+}
