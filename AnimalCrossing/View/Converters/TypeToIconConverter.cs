9c54df07db8995850906c78e3d70a4f232e6489c
Jolan Plaum <jolan.plaum@student.howest.be>
2023-06-03 23:59:48 +0200

    Create start of UI
    Currently the types are shown with their respective icon.
    This will later be used to filter through the critters

---
+++ AnimalCrossing/View/Converters/TypeToIconConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace AnimalCrossing.View.Converters
+{
+	internal class TypeToIconConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+				return new BitmapImage(new Uri("https://acnhcritterpedia.com/icons/icon-transparent.png"));
+
+
+			string type = value.ToString();
+
+			return new BitmapImage(new Uri($"/Resources/Icons/{type}.png", UriKind.Relative));
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ AnimalCrossing/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using AnimalCrossing.Model;
+using AnimalCrossing.Repository;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalCrossing.ViewModel
+{
+	public class MainViewModel : ObservableObject
+	{
+		private CritterLocalRepository _localRepository;
+
+		private string _selectedType;
+		public string SelectedType
+		{
+			get { return _selectedType; }
+			set {
+				_selectedType = value;
+				OnPropertyChanged(nameof(SelectedType));
+
+				CritterList = _localRepository.GetCritters(SelectedType);
+				OnPropertyChanged(nameof(CritterList));
+			}
+		}
+
+		private BaseCritter _selectedCritter;
+		public BaseCritter SelectedCritter
+		{
+			get { return _selectedCritter; }
+			set {
+				_selectedCritter = value;
+				OnPropertyChanged(nameof(SelectedCritter));
+			}
+		}
+
+		public List<string> CritterTypes { get; private set; }
+		public List<BaseCritter> CritterList { get; private set; }
+
+		public MainViewModel()
+		{
+			_localRepository = new CritterLocalRepository();
+
+			CritterList = _localRepository.GetCritters();
+			CritterTypes = _localRepository.GetCritterTypes();
+		}
+	}
+}
